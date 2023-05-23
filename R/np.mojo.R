e22347ec80beb6ba2bb4293ac23d610c5106961a
Euan McGonigle <kz20709@bristol.ac.uk>
2023-05-23 12:25:34 +0100

    update documentation

--- R/np.mojo.R
+++ R/np.mojo.R
@@ -41,8 +41,8 @@
 #' @param criterion String indicating how to determine whether each point \code{k} at which NP-MOJO statistic
 #' exceeds the threshold is a change point; possible values are
 #'  \itemize{
-#'    \item{\code{"epsilon"}}{\code{k} is the maximum of its local exceeding environment, which has at least size \code{epsilon*G}}
-#'        \item{\code{"eta"}}{there is no larger exceeding in an \code{eta*G} environment of \code{k}.}
+#'    \item{\code{"epsilon"}}{: \code{k} is the maximum of its local exceeding environment, which has at least size \code{epsilon*G}}
+#'        \item{\code{"eta"}}{: there is no larger exceeding in an \code{eta*G} environment of \code{k}.}
 #' }
 #' @param eta A positive numeric value for the minimal mutual distance of
 #' changes, relative to bandwidth (if \code{criterion = "eta"}).
@@ -159,7 +159,7 @@ np.mojo <- function(x, G,lag = 0, kernel.f = c("quad.exp", "gauss", "euclidean",
 
   if(data.driven.kern.par == TRUE){
     if(kernel.f == "euclidean"){
-      warning("Data driven parameter choice not suited for this kernel. Parameter kern.par reset to 1.")
+      warning("Data driven parameter choice not suited for Euclidean kernel. Parameter kern.par reset to 1.")
       kern.par <- 1
     }
     else{
--- R/np.mojo.multilag.R
+++ R/np.mojo.multilag.R
@@ -38,8 +38,8 @@
 #' @param criterion String indicating how to determine whether each point \code{k} at which NP-MOJO statistic
 #' exceeds the threshold is a change point; possible values are
 #'  \itemize{
-#'    \item{\code{"epsilon"}}{\code{k} is the maximum of its local exceeding environment, which has at least size \code{epsilon*G}}
-#'        \item{\code{"eta"}}{there is no larger exceeding in an \code{eta*G} environment of \code{k}.}
+#'    \item{\code{"epsilon"}}{: \code{k} is the maximum of its local exceeding environment, which has at least size \code{epsilon*G}}
+#'        \item{\code{"eta"}}{: there is no larger exceeding in an \code{eta*G} environment of \code{k}.}
 #' }
 #' @param eta A positive numeric value for the minimal mutual distance of
 #' changes, relative to bandwidth (if \code{criterion = "eta"}).
